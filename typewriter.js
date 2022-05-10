e44d3f5d1c0d0edd8cb69130b09b69e99539f3da
Boluwatife Ade Ogunlola <dontife@ymail.com>
2022-05-10 19:39:52 +0000

    This code takes in a sentence and display it as an animation

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+/* This code takes in a sentence and display it as an animation
+   This is possible by looping through the string and setting a 
+   delay of 5oms on each char when we print the string 
+*/
+const sentence = 'hello there from lighthouse labs'; 
+let time =  0;
+for (const char of sentence) {
+ // console.log(char);
+  setTimeout(() =>{
+    process.stdout.write(char +'\n');
+  }, time += 50)
+}
